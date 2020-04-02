19a4e78a1cf37745ebb247fd3341c84e2ea3e233
Eiram Araujo <eiramaraujo1998@gmail.com>
2020-04-01 18:13:16 -0600

    Created and completed the addTodo component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ToDosComponent } from './components/to-dos/to-dos.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:ToDosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
